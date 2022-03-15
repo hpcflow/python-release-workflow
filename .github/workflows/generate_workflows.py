b9d02068c491d6cca5f9f064284a2ca0a8d7694c
aplowman <adam.jp@live.co.uk>
2022-03-15 15:20:05 +0000

    black format py

--- .github/workflows/generate_workflows.py
+++ .github/workflows/generate_workflows.py
@@ -5,30 +5,32 @@ import sys
 
 from jinja2 import Template
 
+
 def main(template_dir, vars_json_path):
 
     template_dir = Path(template_dir)
     if not template_dir.is_dir():
-        raise ValueError(f'Not a directory: {str(template_dir)}.')
+        raise ValueError(f"Not a directory: {str(template_dir)}.")
 
-    template_files = list(template_dir.glob('*.in'))
+    template_files = list(template_dir.glob("*.in"))
 
     vars_json_path = Path(vars_json_path)
 
-    with vars_json_path.open('r')as fh:
+    with vars_json_path.open("r") as fh:
         jsonc_str = fh.read()
-        json_str = re.sub("//.*","", jsonc_str, flags=re.MULTILINE)
-        vars = json.loads(json_str)        
-    
+        json_str = re.sub("//.*", "", jsonc_str, flags=re.MULTILINE)
+        vars = json.loads(json_str)
+
     for file_path in template_files:
-        
-        with file_path.open('r') as fh:
+
+        with file_path.open("r") as fh:
             template = Template(fh.read())
 
         rendered = template.render(**vars)
 
-        with file_path.with_suffix('').open('w', newline='\n') as fh:
-            fh.write(rendered + '\n')
+        with file_path.with_suffix("").open("w", newline="\n") as fh:
+            fh.write(rendered + "\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(*sys.argv[1:])
